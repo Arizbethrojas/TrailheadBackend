074c977e9cd2c5b9162eb305300573b38ce5049b
Sammy Rago <samantha.k.rago.25@dartmouth.edu>
2024-11-04 11:15:29 -0500

    added subclub table

--- trailhead/webapp/urls.py
+++ trailhead/webapp/urls.py
@@ -1,6 +1,6 @@
 #here is where we will place different URL routes and then connect them to our views 
 from django.urls import path 
-from .views import TripCreate
+from .views import TripCreate, SubclubList
 from . import views
 
 # list of our paths
@@ -8,5 +8,7 @@ urlpatterns = [
     path("", views.home, name ="home"), #this is an empty path to the base URL of our website
     path("todos/", views.todos, name = "Todos"),
     path('api/trips/', TripCreate.as_view(), name='trip-create'),
+    # path('api/trips/', views.create_trip, name='trip-create'),
     path("trips/create/", views.create_trip, name="create_trip"),
+    path("api/subclubs/", SubclubList.as_view(), name="subclub-list")
 ]
