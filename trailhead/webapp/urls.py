28bc3501b6d6f57db41dedbdfe8c23d6af05ad70
colinkearns2 <colin.r.kearns.25@dartmouth.edu>
2025-02-03 00:21:57 -0500

    profile sync database

--- trailhead/webapp/urls.py
+++ trailhead/webapp/urls.py
@@ -1,13 +1,16 @@
 #here is where we will place different URL routes and then connect them to our views 
 from django.urls import path 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import TripCreate, SubclubList, RegisterTrip, ViewRegistrationsByStudent
+from .views import TripCreate, SubclubList, RegisterTrip, ViewRegistrationsByStudent, StudentProfileView, StudentTripsView, SubclubTripsView
 from . import views
 from django.contrib.auth import views as auth_views
 
 
 # list of our paths
 urlpatterns = [
+    path('api/student/current/', StudentProfileView.as_view()),
+    path('api/student/<int:student_id>/trips/', StudentTripsView.as_view()),
+    path('api/subclub/<int:subclub_id>/trips/', SubclubTripsView.as_view()),
     path("", views.home, name ="home"), #this is an empty path to the base URL of our website
     path("todos/", views.todos, name = "Todos"),
     path("api/trips/", TripCreate.as_view(), name='trip-create'),
