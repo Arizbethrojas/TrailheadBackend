79e249fe1810d865480f8a7e75d981bb7def82c8
Dara Casey <dara.e.casey.25@dartmouth.edu>
2025-02-19 16:45:29 -0500

    chat merge

---
+++ trailhead/webapp/firebase/firebase_admin.py
@@ -0,0 +1,20 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+
+import os
+print("Current working directory:", os.getcwd())
+
+
+
+
+# Path to your service account key file
+cred = credentials.Certificate('path')
+
+
+# Initialize Firebase Admin SDK
+firebase_admin.initialize_app(cred)
+
+
+# Initialize Firestore
+db = firestore.client()  # This is your Firestore database reference
