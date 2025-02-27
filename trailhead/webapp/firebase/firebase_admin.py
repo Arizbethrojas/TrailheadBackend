8f96492cd01569eb06fcde4aa3656e1afdecdd2e
Sammy Rago <samantha.k.rago.25@dartmouth.edu>
2025-02-27 15:41:26 -0500

    removed api key for deployment

--- trailhead/webapp/firebase/firebase_admin.py
+++ trailhead/webapp/firebase/firebase_admin.py
@@ -1,6 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
-
+from dotenv import load_dotenv
 
 import os
 print("Current working directory:", os.getcwd())
@@ -9,7 +9,10 @@ print("Current working directory:", os.getcwd())
 
 
 # Path to your service account key file
-cred = credentials.Certificate('path')
+load_dotenv()
+api_key = os.getenv('FIREBASE_KEY')
+url = 'webapp/firebase/' + api_key
+cred = credentials.Certificate(url)
 
 
 # Initialize Firebase Admin SDK
