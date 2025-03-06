74a22602c9bd2c43ccaa9ac8c54e033c5c42149a
Sammy Rago <samantha.k.rago.25@dartmouth.edu>
2025-03-06 15:26:23 -0500

    Update firebase_admin.py

--- trailhead/webapp/firebase/firebase_admin.py
+++ trailhead/webapp/firebase/firebase_admin.py
@@ -12,7 +12,8 @@ print("Current working directory:", os.getcwd())
 # load_dotenv()
 # api_key = os.getenv('FIREBASE_KEY')
 # url = 'webapp/firebase/' + api_key
-cred = credentials.Certificate("webapp/firebase/trai-47353-firebase-adminsdk-fbsvc-4a91659dff.json")
+#render stores this in the root file
+cred = credentials.Certificate("/trai-47353-firebase-adminsdk-fbsvc-4a91659dff.json")
 
 
 # Initialize Firebase Admin SDK
